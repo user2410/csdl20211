e80435ce5178845ec5e7cc7654265587052ba04c
user2410 <dc241024@gmail.com>
2021-10-27 10:49:47 +0700

    Author's Index/Create/New Routes

--- server.js
+++ server.js
@@ -3,32 +3,21 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const expressLayout = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+const db = require('./db');
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayout);
 app.use(express.static('public'));
-
-const mysql = require('mysql');
-// Create connection
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'csdl20211',
-});
-
-// Connect to MySQL
-db.connect((err)=>{
-    if(err){throw err;}
-    console.log('MySQL connected');
-});
-
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
