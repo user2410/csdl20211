71e8da1659b24c5a765a60e6142d65607a1c3c88
user2410 <dc241024@gmail.com>
2021-12-16 23:25:11 +0700

    finished backend + bootstrap

--- public/scripts/img_preview.js
+++ public/scripts/img_preview.js
@@ -13,29 +13,26 @@ const previewImage = previewContainer.querySelector(".image-preview__image");
 const previewDefaultText = previewContainer.querySelector(".image-preview__default-text");
 const inputForm = document.getElementById("inpForm");
 
+function imgReset(){
+    inpFile.value = "";
+    previewImage.setAttribute("src", "");
+    previewImage.style.display = "none";
+    previewDefaultText.style.display = "flex";
+}
+
 inpFile.addEventListener("change", function(){
     const file = this.files[0];
-    if(file.size > 0x100000){
-        inpFile.value = "";
-        previewImage.setAttribute("src", "");
-        previewDefaultText.style.display = initial;
-        alert(`Image too large ${file.size}`)
-        return;
-    }
     if(file){
+        if(file.size > 0x100000){return imgReset();}
         const reader = new FileReader();
         previewDefaultText.style.display = "none";
         previewImage.style.display = "block";
         reader.addEventListener("load", function(){
-            console.log(this);
             previewImage.setAttribute("src", this.result);
             let data = this.result.split(';base64,');
-            document.getElementById('imgType').value = data.pop();
+            document.getElementById('imgContent').value = data.pop();
             document.getElementById('imgType').value = data.pop().slice(11);
         })
         reader.readAsDataURL(file);
-    } else {
-        previewDefaultText.style.display = null;
-        previewImage.style.display = null;
-    }
+    } else {imgReset();}
 });
